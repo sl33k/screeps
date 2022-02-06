3a84c398a9f7b33e373c9adb7a3d1ca3138b24ae
Malte Bargholz <maltebargholz@gmail.com>
2022-02-06 13:10:06 +0100

    implement energy manager

--- energy-manager.js
+++ energy-manager.js
@@ -1,32 +1,62 @@
 const _ = require('lodash')
+const util = require('util')
 
-function find_all_free_sources() {
-    return []
+class EnergyHarvestSpot {
+    constructor(source, harvest_position) {
+        this.source = source
+        this.harvest_position = harvest_position
+    }
+    /**
+     * Test equality
+     * @returns {boolean} Whether or not its equal.
+     */
+    is_equal(other) {
+        return this.source == other.source && this.harvest_position == other.harvest_position;
+    }
 }
 
+
 class EnergySourceManager {
+    /**
+     * @param {Room} room The room for which it manages the energy
+     */
     constructor(room) {
         this.room = room
         this.free = []
         this.used = []
     }
 
+    /**
+     * Updates the available harvest spots in the room
+     */
     update() {
-        this.free = find_all_free_sources(room)
+        const sources = this.room.find(FIND_SOURCES);
+        var acc = [];
+        for(const s in sources) {
+            const harvest_positions = _.map(s, util.freePositionsAroundObject)
+            const harvest_spots = _.map(harvest_positions, (p) => new EnergyHarvestSpot(s, p))
+            acc.push(...harvest_spots);
+        }
+        this.free = acc;
     }
 
-    get_next() {
-        const actually_free = this.free - this.used;
-        return actually_free[0];
+    /**
+     * @returns {EnergyHarvestSpot} The next free spot to harvest
+     */
+    get() {
+        const actually_free = _.differenceWith(this.free, this.used, (a, b) => a.is_equal(b))
+        if(actually_free.length == 0)
+            return null;
+        const pick = actually_free[0];
+        this.used.push(pick)
+        return pick
     }
 
-    reserve(source) {
-        this.used.push(source);
-    }
-
-    release(source) {
-        const idx = this.used.findIndex(source)
-        this.used.splice(idx, 1)
+    /**
+     * @param {EnergyHarvestSpot} spot The spot to be released
+     */
+    release(spot) {
+        _.pullAllWith(this.used, spot, (a, b) => a.is_equal(b))
     }
 }
 
